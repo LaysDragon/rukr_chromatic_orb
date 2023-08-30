f76261fd6f90699791dde71d959725160129b9d3
LaysDragon <laysdra7265@gmail.com>
2023-08-31 04:44:28 +0800

    chore:disable debug config

--- src/config.ts
+++ src/config.ts
@@ -18,8 +18,8 @@ export default {
       //   y: 9.8
       // },
       debug: {
-        showBody: true,
-        showStaticBody: true
+        // showBody: true,
+        // showStaticBody: true
       }
     }
   },
